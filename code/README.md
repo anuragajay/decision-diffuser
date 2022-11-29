4ec84e52eb972f3295421467e7192aa50a05b263
aajay <aajay@MacBook-Pro-15.local>
2022-11-29 08:43:06 -0600

    added initial code and arxiv link

---
+++ code/README.md
@@ -0,0 +1,3 @@
+## Usage
+
+The codebase is still being updated. We will soon add detailed instructions. In case of any confusion, please email aajay@mit.edu.
---
+++ code/config/locomotion_config.py
@@ -0,0 +1,59 @@
+import torch
+
+from params_proto.neo_proto import ParamsProto, PrefixProto, Proto
+
+class Config(ParamsProto):
+    # misc
+    seed = 100
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    bucket = '/home/gridsan/aajay/weights/'
+    dataset = 'hopper-medium-expert-v2'
+
+    ## model
+    model = 'models.TemporalUnet'
+    diffusion = 'models.GaussianDiffusion'
+    horizon = 100
+    n_diffusion_steps = 100
+    action_weight = 10
+    loss_weights = None
+    loss_discount = 1
+    predict_epsilon = False
+    dim_mults = (1, 4, 8)
+    returns_condition = True
+    calc_energy=False
+    dim=128
+    condition_dropout=0.25
+    condition_guidance_w = 1.2
+    test_ret=1.0
+    renderer = 'utils.MuJoCoRenderer'
+
+    ## dataset
+    loader = 'datasets.SequenceDataset'
+    normalizer = 'CDFNormalizer'
+    preprocess_fns = []
+    clip_denoised = True
+    use_padding = True
+    include_returns = True
+    discount = 0.99
+    max_path_length = 1000
+    hidden_dim = 256
+    ar_inv = False
+    train_only_inv = False
+    termination_penalty = -100
+    returns_scale = 400.0 # Determined using rewards from the dataset
+
+    ## training
+    n_steps_per_epoch = 10000
+    loss_type = 'l2'
+    n_train_steps = 1e6
+    batch_size = 32
+    learning_rate = 2e-4
+    gradient_accumulate_every = 2
+    ema_decay = 0.995
+    log_freq = 1000
+    save_freq = 10000
+    sample_freq = 10000
+    n_saves = 5
+    save_parallel = False
+    n_reference = 8
+    save_checkpoints = False
